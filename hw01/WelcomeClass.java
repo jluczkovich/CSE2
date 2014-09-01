c51dfd4c9b594e61be0119980dfc95414963231b
jluczkovich <jjl318@lehigh.edu>
2014-09-01 03:42:41 +0000

    wrote WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////
+//John Luczkovich
+//hw01
+//Welcome Class Java Program
+//
+//  first compile program
+//      javac WelcomeClass.java
+//  run program
+//      java WelcomeClass
+//
+//define a class
+public class WelcomeClass {
+    
+    //add main method
+    public static void main(String[] args) {
+        
+        //print the statement with tweet autobiography
+        System.out.println("    -----------");  
+        System.out.println("    | WELCOME |");
+        System.out.println("    -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-J--J--L--3--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("If I could spend everyday fishing on the Saint Lawrence River");
+        System.out.println("like I did several years ago, I would be a happy man.");
+        
+        //test
+    }
+}
